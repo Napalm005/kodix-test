6ea840dd52f0d80d9e1b066c1dcc7f398eff90ea
aleksandr.frolov <frolov@bookform.ru>
2019-04-24 11:59:35 +0300

    some cleaning

--- src/components/CardsList/index.js
+++ src/components/CardsList/index.js
@@ -6,41 +6,6 @@ import { connect } from 'react-redux'
 
 class CardsList extends Component {
 
-  getSort (ascending) {
-    // if ascending, `null` will be pushed towards the end of the array by returning 1
-    var nullPosition = ascending ? 1 : -1
-    return function (a, b) {
-      // if a is null, push it towards whichever end null elements should end up
-      if (a == null) return nullPosition
-      
-      // Note: at this point, a is non-null (previous if statement handled that case).
-      //
-      // If b is null, it must therefore be placed closer to whichever end the null
-      // elements should end up on. If ascending, null elements are pulled towards
-      // the right end of the array. If descending, null elements are pulled towards
-      // the left.
-      //
-      // Therefore, we return -nullPosition. If ascending, this is -1, meaning a comes
-      // before b; if descending, this is 1, meaning a comes after b. This is
-      // clearly the correct behavior, since ascending will push b, which is null,
-      // towards the end of the array (with -1) and descending will push b towards
-      // the beginning of the array.
-      if (b == null) return -nullPosition
-  
-      // OTHERWISE, both elements are non-null, so sort normally.
-      // if a < b AND
-      //     if ascending, a comes first, so return -1 == -nullPosition
-      //     if descending, a comes after, so return -nullPosition == -(-1) == 1
-      if (a < b) return -nullPosition
-  
-      // return the opposite of the previous condition
-      if (a > b) return nullPosition
-      
-      // return 0 if both elements are equal
-      return 0
-    }
-  }
-
   renderCards = () => {
     const { data, filter } = this.props;
 
